b70bc41f645a1f184a0a88030b21eb2668cfb3c1
DGAnik <anikofficial753@gmail.com>
2023-07-05 02:01:46 +0600

    cart design almost done and add to cart button is added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-  const {name, img, seller, quantity, price, ratings} = props.pro;
-  console.log(props.pro)
+  const { name, img, seller, quantity, price, ratings } = props.pro;
+  console.log(props.pro);
   return (
-    <div className='product'>
+    <div className="product">
       <img src={img} alt="" />
-      <h3>{name}</h3>
-      <h3>Price: {price}</h3>
-      <p>Manufacturer: {seller}</p>
-      <p>Rating: {ratings}</p>
-
+      <div className="product-info">
+        <h4 className="product-name">{name}</h4>
+        <h3>Price: {price}</h3>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings} Stars</p>
+      </div>
+        <button className="btn-cart">Add to Cart</button>
     </div>
   );
 };
