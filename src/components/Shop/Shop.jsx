964f5202737558c52e0e64ba48ea21a82351bb46
DGAnik <anikofficial753@gmail.com>
2023-07-08 22:53:23 +0600

    data added to the local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Shop.css";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
   
@@ -11,11 +12,17 @@ const Shop = () => {
     .then((res) => res.json())
     .then((data) => setProducts(data));
   }, []);
+
+  useEffect( ()=>{
+    const storedCart = getShoppingCart();
+    console.log(storedCart)
+  },[])
   
   const [cart, setCart] = useState([]);
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
 
   return (
